232de98376865beb1db7bdeffc84728d971c3fe6
Rain Elken <rain.elken@envir.ee>
2016-04-09 20:29:48 +0300

    teine praktikum

---
+++ 2praktikum_koolis_Elken.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8"/>
+		<title>Esimene praktikum</title>
+		<link rel="stylesheet" type="text/css" href="1prax.css">
+	</head>	
+	<body>
+	<br/>
+	<br/>
+		<h1>Kaardirakendus</h1>
+		<p> <i>"Võrgurakendused I: Klient-server süsteemide ehitamine"</i> aine raames proovin luua mõne lihtsama <strong>veebikaardirakenduse</strong>,<br/>
+		mis oleks seotud <b><a href="https://www.mysql.com/">MySQL</a></b> baasiga ning kust oleks võimalik teha:
+		</p>
+				<ul>
+					<li>lihtsamaid (geopäringuid)</li>
+					<li>võimalusel ise uusi ruumiandmeid baasi sisestada</li>
+					<li><em>teha asukohapõhist statistikat</em></li>
+				</ul>
+		<IMG class="displayed" src="http://service.eomap.ee/abi_pildid/infoparingunupuekraan.png" alt="Pilt puudub">
+		<p class="center">Üks näide kaardipildist. Vaatame mis reaalselt välja tuleb...</p>
+	<br/>
+	<br/>
+		<h2>Pean veel õppima:</h2>
+				<ol>
+					<li class="indent">CSS-iga kujundust</li>
+					<li class="indent">JavaScripti</li>
+					<li class="indent">SQL keelt</li>
+					<li class="indent">PHP-d</li>
+				</ol>
+	
+	INSERT INTO relken_test (ip_address) VALUES ('193.40.250.235')test
+	<p>
+	
+
+
+		<a href="http://validator.w3.org/check?uri=referer">
+		<img src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Strict" height="31" width="88" />
+		</a>
+	</p>
+	</body>
+</html>
