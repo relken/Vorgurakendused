2ef74a6b687e46344ca86bb44c2af22def18aaa4
Rain Elken <rain.elken@envir.ee>
2016-04-17 23:15:35 +0300

    9 praktikum

---
+++ 9prax/kontroller.php
@@ -0,0 +1,28 @@
+<?php
+require_once('head.html');
+  
+$dirname = "pildid/";
+$images = glob($dirname."*.jpg");
+  
+
+$page = "";
+if (!empty($_GET['page'])) {
+  $page = $_GET['page'];
+}
+switch($page){
+	case "galerii":
+		include("galerii.html");
+	break;
+	case "vote":
+		include("vote.html");
+	break;
+	case "tulemus":
+		include("tulemus.html");
+	break;
+	default:
+		include("pealeht.html");
+	break;
+}
+
+require_once('foot.html');
+?>
