d1685dc17b1c50ea3a9f105469e1891fedd5a0a9
Rain Elken <rain.elken@envir.ee>
2016-05-08 23:34:05 +0300

    12 prax

---
+++ 12prax/loomaaed/update_database.sql
@@ -0,0 +1,4 @@
+UPDATE rain_loomaaed SET liik="pildid/cat.png" WHERE liik="kass";
+UPDATE rain_loomaaed SET liik="pildid/dog.png" WHERE liik="koer";
+UPDATE rain_loomaaed SET liik="pildid/monkey.png" WHERE liik="konn";
+UPDATE rain_loomaaed SET liik="pildid/rabbit.png" WHERE liik="krokodill";
